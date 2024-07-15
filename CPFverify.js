748d56e8e2c0816ac8abccf6bf9f45f043cb983b
Ericsson Gomes <contato@ericssongomes.com>
2024-07-15 02:11:44 -0300

    update variables

--- CPFverify.js
+++ CPFverify.js
@@ -11,29 +11,29 @@ function CPFvalidate(cpf) {
         return false;
     }
 
-    let soma = 0;
-    let resto;
+    let sum = 0;
+    let rest;
 
     for (let i = 1; i <= 9; i++) {
-        soma += parseInt(cpf.charAt(i - 1)) * (11 - i);
+        sum += parseInt(cpf.charAt(i - 1)) * (11 - i);
     }
-    resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) {
-        resto = 0;
+    rest = (sum * 10) % 11;
+    if (rest === 10 || rest === 11) {
+        rest = 0;
     }
-    if (resto !== parseInt(cpf.charAt(9))) {
+    if (rest !== parseInt(cpf.charAt(9))) {
         return false;
     }
 
-    soma = 0;
+    sum = 0;
     for (let i = 1; i <= 10; i++) {
-        soma += parseInt(cpf.charAt(i - 1)) * (12 - i);
+        sum += parseInt(cpf.charAt(i - 1)) * (12 - i);
     }
-    resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) {
-        resto = 0;
+    rest = (sum * 10) % 11;
+    if (rest === 10 || rest === 11) {
+        rest = 0;
     }
-    if (resto !== parseInt(cpf.charAt(10))) {
+    if (rest !== parseInt(cpf.charAt(10))) {
         return false;
     }
 
